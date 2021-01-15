a05e622562e914dcdfd7d735fc3bd155fee97511
taajuddin <taajraza12@gmail.com>
2021-01-15 17:58:40 +0530

    using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =()=> {
+	
+ const [value,setValue]=useState(0)
+		
+	// const handleUp=()=>setValue(value+1)
+	// const handleDown=()=>setValue(value-1)
+	// const handleReset=()=>setValue(0)
+
+	return (
+		<div className='app'>
+			<h1>Counter App</h1>
+			<div>
+			<b>{value}</b><br />
+			<button onClick={()=>setValue(value+1)}>+</button>
+			<button onClick={()=>setValue(value-1)}>-</button>
+			<button onClick={()=>setValue(0)}>Reset</button>
+			</div>	
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
 
+
+// Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
